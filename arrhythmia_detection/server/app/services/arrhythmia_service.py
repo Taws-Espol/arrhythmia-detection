f7f9b2acd232e71d26c168d0d876fc2961109bb9
JFcoFernandez <juaffern@espol.edu.ec>
2024-03-01 22:40:00 -0500

    Changes to server socketio.on("heartbeat_input") now receives only a number instead of a 186 values array and then two emits

--- arrhythmia_detection/server/app/services/arrhythmia_service.py
+++ arrhythmia_detection/server/app/services/arrhythmia_service.py
@@ -1,11 +1,26 @@
+import json
 from app.models import ArrhythmiaModel
 
 
 class ArrhythmiaService:
     def __init__(self):
         self.arrhythmia_model = ArrhythmiaModel()
+        self.heartbeat_data = []
 
-    def predict_arrhythmia(self, json_data):
+    def predict_arrhythmia(self, number):
         # Perform any necessary processing on heartbeat_data
-        prediction = self.arrhythmia_model.predict_arrhythmia(json_data)
-        return prediction
+        self.heartbeat_data.append(number)
+        if len(self.heartbeat_data) == 186:
+            prediction, class_likelihood = self.arrhythmia_model.predict_arrhythmia(
+                self.heartbeat_data
+            )
+            self.heartbeat_data.clear()
+            class_likelihood = float(class_likelihood)
+            prediction_dict = {
+                "prediction": prediction,
+                "class_likelihood": class_likelihood,
+            }
+            # Serialize the dictionary to a JSON string
+            json_string = json.dumps(prediction_dict)
+            return json_string
+        return "no_prediction"
